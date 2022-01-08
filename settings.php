cef92b410b8dc700ac32dfce7a26220049844f72
Kan-Ru Chen <kanru@kanru.info>
2022-01-08 22:40:50 +0900

    refactor: minor lint warnings clean up

--- settings.php
+++ settings.php
@@ -65,47 +65,47 @@ function section_general_callback($args)
 
 function field_auth_domain_cb($args)
 {
-    if (\defined('WP_CF_ACCESS_AUTH_DOMAIN')) {
-        $auth_domain = WP_CF_ACCESS_AUTH_DOMAIN;
+    if (defined('WP_CF_ACCESS_AUTH_DOMAIN')) {
+        $auth_domain = constant('WP_CF_ACCESS_AUTH_DOMAIN');
         $disabled = true;
     } else {
         $auth_domain = get_option('wpcfajal_auth_domain');
         $disabled = false;
     }
-    ?>
+?>
     <input name="wpcfajal_auth_domain" type="text" id="<?php echo $args['label_for'] ?>" value="<?php echo esc_html_e($auth_domain) ?>" class="regular-text" <?php echo $disabled ? "disabled" : "" ?>>
-    <?php
+<?php
 }
 
 function field_aud_cb($args)
 {
-    if (\defined('WP_CF_ACCESS_JWT_AUD')) {
-        $aud = WP_CF_ACCESS_JWT_AUD;
+    if (defined('WP_CF_ACCESS_JWT_AUD')) {
+        $aud = constant('WP_CF_ACCESS_JWT_AUD');
         $disabled = true;
     } else {
         $aud = get_option('wpcfajal_aud');
         $disabled = false;
     }
-    ?>
+?>
     <input name="wpcfajal_aud" type="text" id="<?php echo $args['label_for'] ?>" value="<?php echo esc_html_e($aud) ?>" class="regular-text" <?php echo $disabled ? "disabled" : "" ?>>
-    <?php
+<?php
 }
 
 function field_redirect_login_page_cb($args)
 {
-    if (\defined('WP_CF_ACCESS_REDIRECT_LOGIN')) {
-        $redirect_login_page = WP_CF_ACCESS_REDIRECT_LOGIN;
+    if (defined('WP_CF_ACCESS_REDIRECT_LOGIN')) {
+        $redirect_login_page = constant('WP_CF_ACCESS_REDIRECT_LOGIN');
         $disabled = true;
     } else {
         $redirect_login_page = get_option('wpcfajal_redirect_login_page');
         $disabled = false;
     }
-    ?>
+?>
     <label for="<?php echo $args['label_for'] ?>">
-    <input name="wpcfajal_redirect_login_page" type="checkbox" id="<?php echo $args['label_for'] ?>" <?php echo $redirect_login_page ? "checked" : "" ?> <?php echo $disabled ? "disabled" : "" ?>>
-    <?php echo __('redirect to Cloudflare Access', 'auto-login-with-cloudflare') ?>
+        <input name="wpcfajal_redirect_login_page" type="checkbox" id="<?php echo $args['label_for'] ?>" <?php echo $redirect_login_page ? "checked" : "" ?> <?php echo $disabled ? "disabled" : "" ?>>
+        <?php echo __('redirect to Cloudflare Access', 'auto-login-with-cloudflare') ?>
     </label>
-    <?php
+<?php
 }
 
 function settings_page()
@@ -125,18 +125,18 @@ function settings_page_html()
     if (!current_user_can('manage_options')) {
         return;
     }
-    ?>
+?>
     <div class="wrap">
-    <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
-    <form action="options.php" method="post">
-    <?php
+        <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+        <form action="options.php" method="post">
+            <?php
 
-    settings_fields('wpcfajal');
-    do_settings_sections('wpcfajal');
-    submit_button(__('Save Settings', 'auto-login-with-cloudflare'));
+            settings_fields('wpcfajal');
+            do_settings_sections('wpcfajal');
+            submit_button(__('Save Settings', 'auto-login-with-cloudflare'));
 
-    ?>
-    </form>
+            ?>
+        </form>
     </div>
-    <?php
+<?php
 }
